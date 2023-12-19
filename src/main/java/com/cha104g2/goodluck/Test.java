0fedc4af570b013726558969f6003e80b6fb8d48
E.W <a071232002@gmail.com>
2023-12-19 22:44:45 +0800

    dependency hibernate & ignore hibernate.cfg.xml

--- src/main/java/com/cha104g2/goodluck/Test.java
+++ src/main/java/com/cha104g2/goodluck/Test.java
@@ -1,7 +1,35 @@
 package com.cha104g2.goodluck;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.*;
+
 public class Test {
+	
 	public static void main (String[] args) {
-		System.out.println("tset");
+		StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+				.configure()
+				.build();
+		
+		SessionFactory factory = new MetadataSources(registry)
+				.buildMetadata()
+				.buildSessionFactory();
+		
+		Session session = factory.openSession();
+		
+		Transaction tx = session.beginTransaction();
+		
+		Dept dept = new Dept();
+		dept.setDeptno(50);
+		dept.setDname("人事部");
+		dept.setLoc("中壢");
+		
+		session.save(dept);
+		
+		tx.commit();
+		session.close();
+		factory.close();
 	}
 }
