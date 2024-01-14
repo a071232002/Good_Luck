d28070e25de0e9e15ed65a87d619bad65d0051fd
CuttleFish <b9804320975@gmail.com>
2024-01-14 22:30:41 +0800

    test buyItem

--- src/main/java/com/ord/service/Impl/OrderServiceImpl.java
+++ src/main/java/com/ord/service/Impl/OrderServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.dtl.model.Dtl;
 import com.ord.dao.OrderDao;
 import com.ord.dto.BuyItem;
 import com.ord.dto.CreateOrderRequest;
@@ -20,8 +21,9 @@ public class OrderServiceImpl implements OrderService{
 	@Autowired
 	private ProDAO proDao;
 	
+
 //	List<>
-	
+
 	
 	
 	@Override
@@ -32,6 +34,20 @@ public class OrderServiceImpl implements OrderService{
 		for(BuyItem buyItem : createOrderRequest.getBuyItemList()) {
 //			TODO轉成proNo
 			List<ProVO> product = proDao.getAll();
+			
+			for(ProVO productItem : product) {
+				Integer proNo = productItem.getProNo();
+				System.out.println("proNo: " + proNo);
+			}
+			
+			int Price = buyItem.getOrdPrice();
+			
+			Dtl DtlVO = new Dtl();
+			DtlVO.setOrdNo(buyItem.getOrdNo()); //訂單編號 Not Null(PK,FK)
+//			DtlVO.setProNo(buyItem); //商品編號 Not Null(PK,FK)
+			DtlVO.setDtlQty(buyItem.getOrdPrice()); //商品訂購數量
+//			DtlVO.setDtlSell(ordNo); //訂購單價
+			
 			
 		}
 		
