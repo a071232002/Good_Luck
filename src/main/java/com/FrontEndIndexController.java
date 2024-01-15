a61360290467cdc66d27d1b8797cb87e3b1b0b0f
CuttleFish <b9804320975@gmail.com>
2024-01-15 20:11:40 +0800

    購物路徑代做

--- src/main/java/com/FrontEndIndexController.java
+++ src/main/java/com/FrontEndIndexController.java
@@ -1,34 +1,18 @@
 package com;
 
-import java.util.List;
-
 import javax.validation.Valid;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import com.emp.dto.EmpRegisterRequest;
-import com.emp.model.Emp;
-import com.emp.service.EmpService;
-import com.rtn.contant.RtnCateGory;
-import com.rtn.dto.RtnQueryParams;
-import com.rtn.dto.RtnRequest;
-import com.rtn.model.Rtn;
+import com.ord.dto.CreateOrderRequest;
 import com.rtn.service.RtnService;
 
 @Validated
@@ -56,6 +40,13 @@ public class FrontEndIndexController {
 //		model.addAttribute("activeNavItemId", activeNavItemId);
 		return "FrontEnd/shop/shop";
 	}
+	
+//	@PostMapping("")
+//	public ResponseEntity<?> createOrder(@PathVariable Integer usedId,
+//										 @RequestBody @Valid CreateOrderRequest createOrderRequest){
+//		Integer orderId = OrderService.
+//	}
+	
 	
 	
 ////	前台登入|前台|
