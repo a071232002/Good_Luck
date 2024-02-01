bd82f29873f27ad2a2832d5ee2f86334601527cf
Eric Wen <148343731+a071232002@users.noreply.github.com>
2024-02-01 11:11:27 +0800

    display style fix, addApo get rentNo

--- src/main/java/com/apo/controller/ApoController.java
+++ src/main/java/com/apo/controller/ApoController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -40,8 +41,9 @@ public class ApoController {
 	LddService lddSvc;
 	
 	@GetMapping("/addApo")
-	public String addApo(ModelMap model) {
+	public String addApo(ModelMap model, @RequestParam("rentNo") String rentNo) {
 		Apo apo = new Apo();
+		apo.setRent(rentSvc.getOneRent(Integer.valueOf(rentNo)));
 		model.addAttribute(apo);
 		return "FrontEnd/apo/addApo";
 	}
